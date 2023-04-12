873eaeb95c0691be97b17fb81fbb9aaa028fc295
xuyongchao <mail_xyc@sina.com>
2023-04-12 13:56:54 +0800

    Initial commit-20230412

--- web/src/views/base/notice_class.vue
+++ web/src/views/base/notice_class.vue
@@ -1,19 +1,19 @@
 <template>
-     <u-page-form :before-event="beforeEvent" @afterEvent="afterEvent"></u-page-form>
+    <u-page-form :before-event="beforeEvent" @afterEvent="afterEvent"></u-page-form>
 </template>
 <script>
-export default {
-     name: "notice_class",
-     data() {
-          return {
-               beforeEvent: {
+    export default {
+        name: "notice_class",
+        data() {
+            return {
+                beforeEvent: {
                     /**
                      * @summary 页面数据保存前操作
                      * @description
                      * @param  record
                      * */
                     async beforeCreate(record) {
-                         return record;
+                        return record;
                     },
                     /**
                      * @summary 页面数据保存前操作
@@ -21,7 +21,7 @@ export default {
                      * @param  record
                      * */
                     async beforeModify(record) {
-                         return record;
+                        return record;
                     },
                     /**
                      * @summary 页面数据复制前操作
@@ -29,7 +29,7 @@ export default {
                      * @param  records
                      * */
                     async beforeCopy(record) {
-                         return record;
+                        return record;
                     },
                     /**
                      * @summary 页面数据展示前操作
@@ -37,7 +37,7 @@ export default {
                      * @param  record
                      * */
                     async beforeDetail(record) {
-                         return record;
+                        return record;
                     },
                     /**
                      * @summary 页面数据删除前操作
@@ -45,7 +45,7 @@ export default {
                      * @param  records
                      * */
                     async beforeRemove(records) {
-                         return records;
+                        return records;
                     },
                     /**
                      * @summary 页面数据提交前操作
@@ -53,7 +53,7 @@ export default {
                      * @param  records
                      * */
                     async beforeSubmit(records) {
-                         return records;
+                        return records;
                     },
                     /**
                      * @summary 页面数据审核前操作
@@ -61,7 +61,7 @@ export default {
                      * @param  records
                      * */
                     async beforeVerify(records) {
-                         return records;
+                        return records;
                     },
                     /**
                      * @summary 页面数据弃审前操作
@@ -69,7 +69,7 @@ export default {
                      * @param  records
                      * */
                     async beforeAbandon(records) {
-                         return records;
+                        return records;
                     },
                     /**
                      * @summary 页面数据状态切换前操作
@@ -77,7 +77,7 @@ export default {
                      * @param  records
                      * */
                     async beforeSwitch(records) {
-                         return records;
+                        return records;
                     },
                     /**
                      * @summary 页面数据打开前操作
@@ -85,7 +85,7 @@ export default {
                      * @param  records
                      * */
                     async beforeOpen(records) {
-                         return records;
+                        return records;
                     },
                     /**
                      * @summary 页面数据关闭前操作
@@ -93,113 +93,113 @@ export default {
                      * @param  records
                      * */
                     async beforeClose(records) {
-                         return records;
+                        return records;
                     },
-               }
-          }
-     },
-     methods: {
-          afterEvent(event, records, extra) {
-               if (this[event]) this[event](records, extra)
-          },
-          /**
-           * @summary 页面数据保存后操作
-           * @description
-           * @param  record
-           * */
-          async afterCreate(record) {
-               console.log(record);
-          },
-          /**
-           * @summary 页面数据保存后操作
-           * @description
-           * @param  record
-           * */
-          async afterModify(record) {
-               console.log(record);
-          },
-          /**
-           * @summary 页面数据展示后操作
-           * @description
-           * @param  record
-           * */
-          async afterDetail(record) {
-               console.log(record);
-          },
-          /**
-           * @summary 页面数据删除后操作
-           * @description
-           * @param  records
-           * */
-          async afterRemove(records) {
-               console.log(records);
-          },
-          /**
-           * @summary 页面数据提交后操作
-           * @description
-           * @param  records
-           * */
-          async afterSubmit(records) {
-               console.log(records);
-          },
-          /**
-           * @summary 页面数据审核后操作
-           * @description
-           * @param  records
-           * */
-          async afterVerify(records) {
-               console.log(records);
-          },
-          /**
-           * @summary 页面数据弃审后操作
-           * @description
-           * @param  records
-           * */
-          async afterAbandon(records) {
-               console.log(records);
-          },
-          /**
-           * @summary 页面数据撤回前操作
-           * @description
-           * @param  records
-           * */
-          async beforeRevoke(records) {
-               return records;
-          },
-          /**
-           * @summary 页面数据撤回后操作
-           * @description
-           * @param  records
-           * */
-          async afterRevoke(records) {
-               console.log(records);
-          },
-          /**
-           * @summary 页面数据状态切换后操作
-           * @description
-           * @param  records
-           * */
-          async afterSwitch(records) {
-               console.log(records);
-          },
-          /**
-           * @summary 页面数据打开后操作
-           * @description
-           * @param  records
-           * */
-          async afterOpen(records) {
-               console.log(records);
-          },
-          /**
-           * @summary 页面数据关闭后操作
-           * @description
-           * @param  records
-           * */
-          async afterClose(records) {
-               console.log(records);
-          }
-     }
-}
+                }
+            }
+        },
+        methods: {
+            afterEvent(event, records, extra) {
+                if (this[event]) this[event](records, extra)
+            },
+            /**
+             * @summary 页面数据保存后操作
+             * @description
+             * @param  record
+             * */
+            async afterCreate(record) {
+                console.log(record);
+            },
+            /**
+             * @summary 页面数据保存后操作
+             * @description
+             * @param  record
+             * */
+            async afterModify(record) {
+                console.log(record);
+            },
+            /**
+             * @summary 页面数据展示后操作
+             * @description
+             * @param  record
+             * */
+            async afterDetail(record) {
+                console.log(record);
+            },
+            /**
+             * @summary 页面数据删除后操作
+             * @description
+             * @param  records
+             * */
+            async afterRemove(records) {
+                console.log(records);
+            },
+            /**
+             * @summary 页面数据提交后操作
+             * @description
+             * @param  records
+             * */
+            async afterSubmit(records) {
+                console.log(records);
+            },
+            /**
+             * @summary 页面数据审核后操作
+             * @description
+             * @param  records
+             * */
+            async afterVerify(records) {
+                console.log(records);
+            },
+            /**
+             * @summary 页面数据弃审后操作
+             * @description
+             * @param  records
+             * */
+            async afterAbandon(records) {
+                console.log(records);
+            },
+            /**
+             * @summary 页面数据撤回前操作
+             * @description
+             * @param  records
+             * */
+            async beforeRevoke(records) {
+                return records;
+            },
+            /**
+             * @summary 页面数据撤回后操作
+             * @description
+             * @param  records
+             * */
+            async afterRevoke(records) {
+                console.log(records);
+            },
+            /**
+             * @summary 页面数据状态切换后操作
+             * @description
+             * @param  records
+             * */
+            async afterSwitch(records) {
+                console.log(records);
+            },
+            /**
+             * @summary 页面数据打开后操作
+             * @description
+             * @param  records
+             * */
+            async afterOpen(records) {
+                console.log(records);
+            },
+            /**
+             * @summary 页面数据关闭后操作
+             * @description
+             * @param  records
+             * */
+            async afterClose(records) {
+                console.log(records);
+            }
+        }
+    }
 </script>
 
 <style scoped>
